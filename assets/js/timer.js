4abaed3dd7e5264e218867316e3d6da817292b92
bman013 <brodie.carter@division5.io>
2024-01-04 19:08:03 +1000

    Updated timer

---
+++ assets/js/timer.js
@@ -0,0 +1,26 @@
+var startDate = new Date();
+var endDate = new Date("Sat Jul 13 2024 09:00:00 GMT+1000")
+
+var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+
+var upgradeTime = 16569600;
+var secondsss = upgradeTime;
+function timer() {
+  var days        = Math.floor(seconds/24/60/60);
+  var hoursLeft   = Math.floor((seconds) - (days*86400));
+  var hours       = Math.floor(hoursLeft/3600);
+  var minutesLeft = Math.floor((hoursLeft) - (hours*3600));
+  var minutes     = Math.floor(minutesLeft/60);
+  var remainingSeconds = seconds % 60;
+  function pad(n) {
+    return (n < 10 ? "0" + n : n);
+  }
+  document.getElementById('countdown').innerHTML = pad(days) + "d " + pad(hours) + "h " + pad(minutes) + "m " + pad(Math.floor(remainingSeconds)) + "s";
+  if (seconds == 0) {
+    clearInterval(countdownTimer);
+    document.getElementById('countdown').innerHTML = "It's show time!";
+  } else {
+    seconds--;
+  }
+}
+var countdownTimer = setInterval('timer()', 1000);
